4c2afb09f01c58d935e8d34ef2197c2e3a2d1c2b
Crowdbotics <team@crowdbotics.com>
2022-01-28 05:53:23 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "zipp_wise_33217",
+  url: "https://zipp_wise_33217.botics.co",
+  api: "https://zipp_wise_33217.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
